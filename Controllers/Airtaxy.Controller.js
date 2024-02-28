678bd2f64e79373d79e71e6ad477c2e0612682da
simplysekinah <akinolasekinat123@gmail.com>
2024-02-28 13:30:38 +0100

    locate

--- Controllers/Airtaxy.Controller.js
+++ Controllers/Airtaxy.Controller.js
@@ -114,10 +114,10 @@ const bookflight = async (request, response) => {
         const email = VerifyToken(token);
         const verifyUser = await userModel.findOne({ email: email })
         if (!verifyUser) return response.status(400).send({ message: "Bad request", status: false })
-        console.log(destinationModel);
-        const locate = await destinationModel.find({ from: from, to: to })
+        console.log({destination_tbs},'');
+        const locate = await destinationModel.find({ from: from , to: to })
         console.log(locate,'model');
-        if (!locate) return response.status(404).send({ message: "Destination not found", status: false })
+        if (locate.length === 0) return response.status(404).send({ message: "Destination not found", status: false })
 
         const bookFlight = await BookflightModel.create({
             from,
